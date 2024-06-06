6c96b7cedf5fc2318aa0b083436a39cb37f3a2d6
solfrak <laporte.samuel2000@gmail.com>
2024-06-06 19:26:11 -0400

    Add premake file

---
+++ premake5.lua
@@ -0,0 +1,14 @@
+﻿project "spdlog"
+    kind "StaticLib"
+    language "C++"
+    targetdir "bin/%{cfg.buildcfg}"
+    
+    files
+    { 
+        "include/**.h"
+    }
+
+    includedirs
+    {
+        "include"
+    }
